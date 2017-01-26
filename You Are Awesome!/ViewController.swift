b0a62059fd7b4d3b04e01039bdf535f025c0ac4a
CSOM <csom@bc-macbookpro-38194d.bc.edu>
2017-01-26 14:59:17 -0500

    The code should be complete

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var MessageLabel: UILabel!
+    
+    @IBOutlet weak var MessageButton: UIButton!
+
+    var index = 0
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,40 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func MessageButtonPressed(_ sender: UIButton) {
+        
+        let messages = ["You Are Fantastic!",
+                        "You Are Great!",
+                        "You Are Amazing!",
+                        "You Are Awesome!",
+                        "When the Genius Bar Needs Help, They Call You!",
+                        "You Brighten My Day!",
+                        "I Can't Wait To Use Your App!"]
+        
+        
+        MessageLabel.text = messages[index]
+        index = index+1
+        
+        if index == messages.count-1{
+            index = 0
+        } else {
+            index = index+1
+        }
+        
+        
+        /*
+        //let message1 = "You Are Da Bomb!"
+        //let message2 = "You Are Great!"
+        //let message3 = "You Are Amazing!"
+        
+        
+        if MessageLabel.text == message1 {
+            MessageLabel.text = message2
+        } else if MessageLabel.text == message2 {
+            MessageLabel.text = message3
+        } else {
+            MessageLabel.text = message1
+        }
+ */
+    }
 }
-
